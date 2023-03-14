8125a4fce790e3e661cc36fabfe2c6df4dc904e1
Levantine-1 <github@levantine.io>
2023-03-14 06:49:26 -0700

    Initial commit of thisper

---
+++ thisper.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+import requests
+import logging
+import os
+
+# jenkins_server = os.getenv("JENKINS_SERVER")
+jenkins_server = "jenkins.levantine.io"
+
+app = Flask(__name__)
+
+
+@app.route('/build', methods=['POST'])
+def make_request():
+    job = request.form['job_id'].strip()  # Get data from request
+    auth_key = request.form['auth_key'].strip()
+
+    # Sanitize Inputs
+    job = job.replace('/', '')
+    auth_key = auth_key.replace('/', '')  # No slashes as it's passed in as an url parameter so at risk for url hijack
+
+    url = "http://" + "github_actions_bmt:" + auth_key + "@" + jenkins_server + "/job/" + job + "/build"
+    app.logger.info(url)
+    response = requests.post(url)
+    return response.text
+
+
+if __name__ == '__main__':
+    app.logger.setLevel(logging.INFO)
+    app.run()
