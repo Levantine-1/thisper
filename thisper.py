5cbc302bfa1790b28faf95924c534d80efd0bee7
Levantine <github@levantine.io>
2024-04-29 18:26:09 -0700

    initial commit

--- thisper.py
+++ thisper.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
 import requests
 import logging
-import socket
-import subprocess
+import re
+import time
 
 jenkins_server = "jenkins.internal.levantine.io"
 app = Flask(__name__)
@@ -14,7 +14,7 @@ def startup_code():
 
 @app.route('/', methods=['GET'])
 def default_response():
-    response = "There will be a health check soon, for now just send things to /build"
+    response = "Hello and welcome! Please use the /build endpoint to trigger a Jenkins job."
     return response
 
 
@@ -24,13 +24,48 @@ def acme_challenge(token):
     return token
 
 
-# Pass in the following parameters:
-#   <job_id> = name of jenkins job
-#   <auth_key> = Jenkins api authorization key
-@app.route('/build', methods=['POST'])
-def make_request():
-    data = request.get_json()
-    job = data['job_id'].strip()  # Get data from request
+def run_jenkins_job(url):
+    try:
+        requests.post(url)
+        job_console_text_url, ec = wait_for_jenkins_job(url)
+        response_text = requests.get(job_console_text_url).text
+        response = response_text, ec
+    except requests.exceptions.ConnectionError:
+        warning_msg = "Jenkins host may not have been configured correctly"
+        app.logger.warning(warning_msg)
+        response = warning_msg, ec = 500
+    return response, ec
+
+
+def wait_for_jenkins_job(url):
+    pattern = r'(.*?)/buildWithParameters.*'
+    job_url = re.sub(pattern, r'\1', url)
+    last_build_url = job_url + "/lastBuild/api/json"
+
+    last_job = requests.get(last_build_url)
+    job_id = last_job.json()['id']
+    job_url = job_url + "/" + job_id
+    job_console_text_url = job_url + "/consoleText"
+
+    inprogress = True
+    max_tries = 120  # 120 tries every 5 seconds = 10 minutes
+    while inprogress != "false" and max_tries > 0:
+        last_job = requests.get(job_url + "/api/json")
+        inprogress = last_job.json()['inProgress']
+        max_tries -= 1
+        if inprogress == "false":
+            if last_job.json()['result'] == "SUCCESS":
+                app.logger.info("Job Completed Successfully")
+                return job_console_text_url, 200
+            else:
+                app.logger.error("Job Failed")
+                return job_console_text_url, 500
+        time.sleep(5)
+    app.logger.error("Thisper timed out waiting for Jenkins job to complete.")
+    return job_console_text_url, 500
+
+
+def sanitize_inputs(data):
     auth_key = data['auth_key'].strip()
     auth_usr = data['auth_usr'].strip()
     services = data['services'].strip()
@@ -40,21 +75,31 @@ def make_request():
         auth_key = "Invalid_Key"
 
     # Sanitize Inputs
-    job = job.replace('/', '')  # No slashes as it's passed in as an url parameter so at risk for url hijack
     auth_usr = auth_usr.replace('/', '')
     auth_key = auth_key.replace('/', '')
     services = services.replace('/', '')
+    return auth_usr, auth_key, services
 
-    url = "http://" + auth_usr + ":" + auth_key + "@" + jenkins_server + "/job/" + job + "/buildWithParameters?services=" + services
+# Pass in the following parameters:
+#   <job_id> = name of jenkins job
+#   <auth_key> = Jenkins api authorization key
+@app.route('/deploy_container', methods=['POST'])
+def deploy_container():
+    data = request.get_json()
+    auth_usr, auth_key, services = sanitize_inputs(data)
+    url = "http://" + auth_usr + ":" + auth_key + "@" + jenkins_server + "/job/DeployContainer/buildWithParameters?services=" + services
     app.logger.info(url.replace(auth_key, '<REDACTED>'))
+    response, ec = run_jenkins_job(url)
+    return response, ec
 
-    try:
-        response = requests.post(url)
-    except requests.exceptions.ConnectionError:
-        warning_msg = "Jenkins host may not have been configured correctly"
-        app.logger.warning(warning_msg)
-        response = warning_msg
-    return response.text if hasattr(response, 'text') else response
+@app.route('/run_terraform', methods=['POST'])
+def run_terraform(auth_usr, auth_key, services):
+    data = request.get_json()
+    auth_usr, auth_key, services = sanitize_inputs(data)
+    url = "http://" + auth_usr + ":" + auth_key + "@" + jenkins_server + "/job/terraform/terraform_" + services + "/buildWithParameters?COMMAND=apply -auto-approve --var-file=./vars/&VAR_FILE=production.tfvars"
+    app.logger.info(url.replace(auth_key, '<REDACTED>'))
+    response, ec = run_jenkins_job(url)
+    return response, ec
 
 
 if __name__ == '__main__':  # These steps will only run if the app is started manually like "/bin/python thisper.py"
