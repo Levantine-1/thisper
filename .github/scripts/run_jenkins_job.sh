86d779af894576c3ff69447dd987c3bf37a5673b
Levantine <github@levantine.io>
2024-05-03 00:05:22 -0700

    caught more unknown status codes and reorganized some logic

--- .github/scripts/run_jenkins_job.sh
+++ .github/scripts/run_jenkins_job.sh
@@ -34,6 +34,20 @@ poll_job_status(){
 
   echo "$poll_path - $service_name - jenkinsJobID: $job_id started, monitoring job status ..."
   while true; do
+    rc=$(eval curl --request GET --location "${url}" --header "'"${header}"'" --data "'"${data}"'" -w "%{http_code}" -o /dev/null --silent)
+    if [[ $rc -eq 200 ]]; then
+      echo "Job completed successfully, outputting logs ..."
+      eval curl --request GET --location "${url}" --header "'"${header}"'" --data "'"${data}"'" --silent
+      break
+    elif [[ $rc -eq 202 ]]; then
+      echo "Job in progress, please wait ..."
+    elif [[ $rc -eq 503 ]]; then
+      echo "Thisper did not respond. This could be because Thisper is restarting in the case of a deployment."
+      echo "If this continues, please check the Thisper service."
+    else
+      echo "Unknown error, server returned HTTP status code: ${rc} - Retrying ..."
+    fi
+
     current_time=$(date +%s)
     elapsed_time=$((current_time - start_time))
     if [[ $elapsed_time -ge $timeout ]]; then
@@ -41,27 +55,7 @@ poll_job_status(){
       eval curl --request GET --location "${url}" --header "'"${header}"'" --data "'"${data}"'" --silent
       exit 1
     fi
-
-    rc=$(eval curl --request GET --location "${url}" --header "'"${header}"'" --data "'"${data}"'" -w "%{http_code}" -o /dev/null --silent)
-    if [[ $rc -eq 200 ]]; then
-      echo "Job completed successfully, outputting logs ..."
-      eval curl --request GET --location "${url}" --header "'"${header}"'" --data "'"${data}"'" --silent
-      break
-
-    elif [[ $rc -eq 202 ]]; then
-      echo "Job in progress, please wait ..."
-      sleep 5
-
-    elif [[ $rc -eq 503 ]]; then
-      echo "Thisper did not respond. This could be because Thisper is restarting in the case of a deployment."
-      echo "If this continues, please check the Thisper service."
-      sleep 5
-
-    else
-      echo "Job failed. Check Jenkins logs for more information."
-      eval curl --request GET --location "${url}" --header "'"${header}"'" --data "'"${data}"'" --silent
-      exit 1
-    fi
+    sleep 5
   done
 }
 
